ed013fdf1d9f5c1acc2b63ae38f318f93dd086b2
Chuk Shirley <chukshirley@gmail.com>
2021-01-12 14:23:15 -0600

    Locate IFS objects within k3s directory

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 BASEDIR=$(pwd);
 DDL=$(cat "${BASEDIR}/DDL/LIFTING.SQL");
 LIBRARY="HERC";
-UTILITIESDIR="/usr/local";
+UTILITIESDIR="/usr/local/k3s/utilities";
 
 # Copy IFS objects to utilities directory
 cp -rf "${BASEDIR}" "${UTILITIESDIR}";
